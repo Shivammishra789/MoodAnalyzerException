d1e356c1ad9cbe59b8934376d92cdc9cf474bb2f
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-12 12:55:19 +0530

    ADDED: I am in any mood should return happy test case

--- src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
@@ -10,8 +10,19 @@ public class MoodAnalyzerTest {
 	public void givenMessage_Whensad_ShouldReturnSad() {
 		
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		String message = "This is sad message";
+		String message = "I am in sad mood";
 		String expectedValue = "SAD";
+		String mood = moodAnalyzer.analyzeMood(message);
+		
+		Assert.assertEquals(expectedValue, mood);
+		
+	}
+	@Test
+	public void givenMessage_AnyMsg_ShouldReturnHappy() {
+		
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String message = "I am in any mood";
+		String expectedValue = "HAPPY";
 		String mood = moodAnalyzer.analyzeMood(message);
 		
 		Assert.assertEquals(expectedValue, mood);
